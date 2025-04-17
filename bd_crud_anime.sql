989eae2a7eeeacf5b34d5e102e5f4cdd916f3137
Jesus Eduardo Yataco Palacios <jesuseduardoyatacopalacios339@gmail.com>
2025-04-17 16:03:48 -0500

    BD agregada

---
+++ bd_crud_anime.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE crudanime;
+USE crudanime;
+
+CREATE TABLE animes(
+	id   			INT AUTO_INCREMENT PRIMARY KEY,
+    titulo 			VARCHAR(50),
+    genero			VARCHAR(100),
+    episodios		INT NOT NULL,
+    creador			VARCHAR(100),
+    fecha_estreno	DATE,
+    calificacion	TINYINT
+    
+)ENGINE = INNODB;
+
+INSERT INTO animes (titulo, genero, episodios, creador, fecha_estreno, calificacion) VALUES 
+	('Charlotte', 'Comedia dramática, sobrenatural', 13, 'Jun Maeda', '2015-07-04', 10);    
+SELECT * FROM animes;
+DROP DATABASE crudanime;
