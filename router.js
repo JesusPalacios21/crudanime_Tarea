778fbad2bd8112e5558c9280743d0299c676cc23
Jesus Eduardo Yataco Palacios <jesuseduardoyatacopalacios339@gmail.com>
2025-04-17 16:00:42 -0500

    Proyecto finalizado

--- router.js
+++ router.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 //2
 // Ruta para mostrar el formulario de creación de un anime
 router.get('/create', (req, res) => {
-    res.render('create'); 
+    res.render('create');
 });
 
 router.post('/create', (req, res) => {
@@ -35,5 +35,54 @@ router.post('/create', (req, res) => {
         res.redirect('/');
     });
 });
+
+//3
+// Ruta para mostrar el formulario de edición
+router.get('/edit/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('SELECT * FROM animes WHERE id = ?', [id], (err, resultado) => {
+        if (err) {
+            console.error(err);
+            return res.send('Error al cargar el anime');
+        }
+        res.render('edit', { anime: resultado[0] });
+    });
+});
+
+
+// Ruta para procesar los cambios del formulario de edición
+router.post('/update/:id', (req, res) => {
+    const id = req.params.id;
+    const { titulo, genero, episodios, creador, fecha_estreno, calificacion } = req.body;
+
+    const query = `UPDATE animes 
+                   SET titulo='${titulo}', genero='${genero}', episodios=${episodios},
+                       creador='${creador}', fecha_estreno='${fecha_estreno}', calificacion=${calificacion} 
+                   WHERE id=${id}`;
+
+    db.query(query, (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.send('Error al actualizar el anime');
+        }
+        res.redirect('/');
+    });
+});
+
+//4
+// Ruta para eliminar un anime
+router.get('/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.query('DELETE FROM animes WHERE id = ?', [id], (err, result) => {
+        if (err) {
+            console.error(err);
+            return res.send('Error al eliminar el anime');
+        }
+        res.redirect('/');
+    });
+});
+
+
 
 module.exports = router;
